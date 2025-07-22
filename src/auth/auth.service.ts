89c27f823741eaa8502b17cb9bb2fa643d316fb9
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2025-07-22 21:56:54 +0600

    config module, passport with jwt token setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
@@ -18,11 +24,8 @@ export class AuthService {
         },
       });
 
-      // remove hash from the returned user object (without mutating the original)
-      const { hash: _, ...userWithoutHash } = user;
-      // delete user.hash;
       // return the saved user
-      return userWithoutHash;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -47,8 +50,25 @@ export class AuthService {
     // if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
     //send back the user
-    const { hash: _, ...userWithoutHash } = user;
-    //delete user.hash;
-    return userWithoutHash;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
