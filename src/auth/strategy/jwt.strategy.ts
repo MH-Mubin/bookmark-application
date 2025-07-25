f86530f960f9fae5a3311ce3f275ec26e82e476d
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2025-07-25 23:22:30 +0600

    update on implementing guards

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     const jwtSecret = config.get<string>('JWT_SECRET');
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined in the environment');
@@ -17,8 +21,19 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
   // Required by Passport JWT
   // validate() is required for all strategies.
-  validate(payload: any) {
-    console.log({ payload });
-    return payload;
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    if (!user) return null;
+
+    // remove `hash` without using delete
+    const { hash, ...userWithoutHash } = user;
+    return userWithoutHash;
+
+    // delete user.hash;
+    // return user;
   }
 }
