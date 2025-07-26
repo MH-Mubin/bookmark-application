71f4986f906614b3fd7c18d972a69870171980f2
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2025-07-26 09:29:28 +0600

    implementing guards and custom param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
+  @Patch()
+  editUser() {}
 }
